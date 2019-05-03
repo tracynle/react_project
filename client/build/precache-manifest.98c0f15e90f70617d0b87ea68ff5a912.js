23ddc73cf7b35be8733e6e1fb7cb7cf7664fe5aa
Tracy Le <tnle9@asu.edu>
2019-05-02 20:57:07 -0700

    Added express and edited package.json

---
+++ client/build/precache-manifest.98c0f15e90f70617d0b87ea68ff5a912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633274202700cd34da630d0de3330ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c07348acbba5db86393",
+    "url": "/static/css/main.a6558988.chunk.css"
+  },
+  {
+    "revision": "771f02aa343883ce4d5d",
+    "url": "/static/js/2.f4c3d5c8.chunk.js"
+  },
+  {
+    "revision": "4c07348acbba5db86393",
+    "url": "/static/js/main.5ce596ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
