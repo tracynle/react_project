ba31519a3473af3f0e483826c9c7c2e8e0b8edae
Tracy Le <tnle9@asu.edu>
2019-05-04 14:11:13 -0700

    Fixed packages dependencies and server port

---
+++ client/build/precache-manifest.4f3a951d334213295799a268dfa4132d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431cae6dd99e99eadbbd3c5170d5bdd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a07e132c463900d7ee",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "30be592dc35ea845e8e4",
+    "url": "/static/js/2.c1aa767d.chunk.js"
+  },
+  {
+    "revision": "29a07e132c463900d7ee",
+    "url": "/static/js/main.39e15074.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
