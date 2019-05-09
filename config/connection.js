7c103ad09e0255829e1555f2c7d6680a5fb72a36
notmsc <mscaro22@gmail.com>
2019-05-09 16:46:36 -0700

    created a .env file and added info

--- config/connection.js
+++ config/connection.js
@@ -11,8 +11,8 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: 'MattPassword',
-        database: 'react_project_db'
+        password: process.env.MSQL_PASS,
+        database: process.env.MYSQL_DB
     })
 };
 
