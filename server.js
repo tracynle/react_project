e71a0d39bdd525162d84f9975ff3365eefc3e1d9
Tracy Le <tnle9@asu.edu>
2019-05-20 22:09:40 -0700

    Made user data persist

--- server.js
+++ server.js
@@ -34,10 +34,11 @@ app.get("*", (req, res) => {
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
+
 })
 .catch((err) => {
     console.log(err);
